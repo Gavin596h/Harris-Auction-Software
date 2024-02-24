afc1f0094fb4ffd6c7ed489a088c9deb489d4d2f
luismaguilar <luis.agui10502@gmail.com>
2024-02-24 12:13:50 -0600

    Grid Layout

--- auction-software-project/src/pages/NewAuction.js
+++ auction-software-project/src/pages/NewAuction.js
@@ -1,3 +1,10 @@
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../style/NewAuction.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function NewAuction() {
     const handleSubmit = async (event) => {
         console.log(event.target); // Debugging line
@@ -24,34 +31,73 @@ function NewAuction() {
 
     return <>
         <form onSubmit={handleSubmit}>
-            <fieldset>
-                <legend>Description</legend><br/>
-                <label for="AuctionName">Auction Name</label>
-                <input type="text" id="AuctionName" name="AuctionName"></input>
-                <label for="AuctionNumber">Auction #</label>
-                <input type="number" id="AuctionNumber" name="AuctionNumber"></input><br/>
-                <label for="ActionDate">Auction Date</label>
-                <input type="date" id="AuctionDate" name="AuctionDate"></input>
-                <label for="TractQuantity">Tract Quantity</label>
-                <input type="number" id="TractQuantity" name="TractQuantity"></input><br/>
-                <label for="UnitOfMeasurement">Unit of Measure (U/M)</label>
-                <select id="UnitOfMeasurement" name="UnitOfMeasurement" size="2">
-                    <option value="Acre">Acre</option>
-                    <option value="placeholder">placeholder</option>
-                </select>
-                <label for="NumOfDecPlaces">No. of U/M Decimal Places</label>
-                <input type="number" id="NumOfDecPlaces" name="NumOfDecPlaces"></input><br/>
-                <label for="CollectiveUnitOfMeasurement">Collective U/M</label>
-                <select id="CollectiveUnitOfMeasurement" name="CollectiveUnitOfMeasurement" size="2">
-                    <option value="Tract">Tract</option>
-                    <option value="placeholder">placeholder</option>
-                </select>
-                <input type="submit" id="TractSetup" name="TractSetup" value="Tract Setup"></input><br/>
-            </fieldset><br/>
+          <div className='container'>
+              <Container>
+              <legend>Description</legend>
+                <Row>
+                  <Col>
+                    <label for="AuctionName">Auction Name</label>
+                    <input type="text" id="AuctionName" name="AuctionName" class='ml-3' ></input>
+                  </Col>
+                  <Col>    
+                    <label for="AuctionNumber" class='ml-3'>Auction #</label>
+                    <input type="number" id="AuctionNumber" name="AuctionNumber" class='ml-3'></input><br/>
+                  </Col>
+                  <Col>
+                    <label for="ActionDate" >Auction Date</label>
+                    <input type="date" id="AuctionDate" name="AuctionDate" class='ml-3'></input>
+                  </Col>           
+                </Row>
+                <br />
+                <Row>
+                <Col>
+                  <label for="TractQuantity">Tract Quantity</label>
+                  <input type="number" id="TractQuantity" name="TractQuantity"  class='ml-3'></input><br/>
+                  
+                  <label for="UnitOfMeasurement">Unit of Measure (U/M)</label>
+                  <select id="UnitOfMeasurement" name="UnitOfMeasurement">
+                      <option value="Acre">Acre</option>
+                      <option value="placeholder">placeholder</option>
+                  </select>
+                  </Col>
+                  <Col>
+                    <label for="NumOfDecPlaces">No. of U/M Decimal Places</label>
+                    <input type="number" id="NumOfDecPlaces" name="NumOfDecPlaces"  class='ml-3'></input><br/>
+                    <label for="CollectiveUnitOfMeasurement">Collective U/M</label>
+                    <select id="CollectiveUnitOfMeasurement" name="CollectiveUnitOfMeasurement">
+                        <option value="Tract">Tract</option>
+                        <option value="placeholder">placeholder</option>
+                    </select>
+                  </Col>
+                  <Col>
+                    <input type="submit" id="TractSetup" name="TractSetup" value="Tract Setup"></input><br/>
+                  </Col>
+                </Row>
+              </Container>
+
+            <br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <fieldset>
                 <legend>Settings</legend>
                 <label for="BidMethod">Bid Method</label>
-                <select id="BidMethod" name="BidMethod" size="2">
+                <select id="BidMethod" name="BidMethod">
                     <option value="InTotal">In Total</option>
                     <option value="placeholder">placeholder</option>
                 </select>
@@ -62,7 +108,7 @@ function NewAuction() {
                 <input type="checkbox" id="WarnOnCombination" name="WarnOnCombination"></input>
                 <label for="WarnOnCombination">Warn on new combination</label><br/>
                 <label for="HighColumn">"High" column description</label>
-                <select id="HighColumn" name="HighColumn" size="2">
+                <select id="HighColumn" name="HighColumn">
                     <option value="High">High</option>
                     <option value="placeholder">placeholder</option>
                 </select>
@@ -70,6 +116,7 @@ function NewAuction() {
                 <label for="BidQueryCombination">Bid query combination</label><br/>
                 <input type="button" id="SetAsDefault" name="SetAsDefault" value="Set as default"></input><br/>
             </fieldset><br/>
+
             <fieldset>
                 <legend>Terms</legend>
                 <input type="checkbox" id="BuyersPrem" name="BuyersPrem"></input>
@@ -85,6 +132,7 @@ function NewAuction() {
                 <input type="radio" id="Amount" name="group1" value="Amount" />
                 <label for="Amount">Amount</label><br/>
             </fieldset><br/>
+
             <fieldset>
                 <legend>System</legend>
                 <input type="button" id="CreateDuplicate" name="CreateDuplicate" value="Create Duplicate"></input>
@@ -92,6 +140,7 @@ function NewAuction() {
                 <input type="button" id="ResetToDefaults" name="ResetToDefaults" value="Reset To Defaults"></input>
                 <input type="button" id="ScreenTitle" name="ScreenTitle" value="Screen Title"></input><br/>
             </fieldset><br/>
+
             <fieldset>
                 <legend>Find Auction</legend>
                 <select id="PastAuctions" name="PastAuctions" size="2">
@@ -106,6 +155,7 @@ function NewAuction() {
                 <input type="radio" id="AuctionDateSearch" name="group2" value="Auction Date" />
                 <label for="AuctionDateSearch">Auction Date</label>
             </fieldset>
+            </div>
         </form>
     </>
 }
