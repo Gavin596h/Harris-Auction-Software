048e689925284709329ba28c54cf6845e526e4e7
luismaguilar <luis.agui10502@gmail.com>
2024-02-29 23:15:24 -0600

    Update bid board

--- auction-software-project/src/pages/BidBoard.js
+++ auction-software-project/src/pages/BidBoard.js
@@ -3,54 +3,56 @@ import React from 'react';
 function BidBoard() {
 
   return (
-    <div className="w-full flex relative overflow-x-auto p-4 sm:ml-64 bg-black ">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-white">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-white">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                    Bidder
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Bid Amount
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    To Lead
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Per Acre
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    High
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Tract
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr className=" dark:bg-black">
-                <th scope="row" className="px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
-                    Owen
-                </th>
-                <td className="px-6 py-4">
-                    $1
-                </td>
-                <td class="px-6 py-4">
-                    $5
-                </td>
-                <td className="px-6 py-4">
-                    $0.75
-                </td>
-                <td className="px-6 py-4">
-                    $1
-                </td>
-                <td className="px-6 py-4">
-                    5
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+    <div className="w-full">
+        <div className="w-full flex justify-center p-4 sm:ml-64 bg-black ">
+            <table className="text-sm text-gray-500 dark:text-white table-auto">
+                <thead className="w-full text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-white">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Bidder
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Bid Amount
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            To Lead
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Per Acre
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            High
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Tract
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            Owen
+                        </th>
+                        <td className="px-6 py-4">
+                            $1
+                        </td>
+                        <td class="px-6 py-4">
+                            $5
+                        </td>
+                        <td className="px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="px-6 py-4">
+                            $1
+                        </td>
+                        <td className="px-6 py-4">
+                            5
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
   );
 }
 
