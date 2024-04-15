c70aabc5abf60a4e05a29cfb1b9ae52c97e2d117
luismaguilar <luis.agui10502@gmail.com>
2024-04-15 08:34:08 -0500

    Update BidBoard

--- auction-software-project/src/pages/BidBoard.js
+++ auction-software-project/src/pages/BidBoard.js
@@ -10,6 +10,7 @@ function BidBoard() {
   let location = useLocation();
   console.log(location);
   const [currentAuction, setCurrentAuction] = useState();
+  const [high, setHigh] = useState();
 
   const ref = useRef();
 
@@ -27,6 +28,12 @@ function BidBoard() {
     }
   };
 
+  const isHigh = (b) => {
+    if(b.High) {
+        setHigh(b);
+    }
+  }
+
 
   useEffect(() => {
     const auctionNumber = localStorage.getItem('currentAuctionNumber');
@@ -39,17 +46,33 @@ function BidBoard() {
     console.log("here I am");
   }, []);
 
+  const SettlementPrint = React.forwardRef((props, refS) => {
+    return(
+        <div className="p-7 font-fira h-screen" refS={refS}>
+        <h2 className='text-black'>Test Auction</h2>
+            <p>April 1, 2024</p>
+            <hr></hr>
+            <p>Bid No. - {high.Bidder}</p>
+            <p>Bid Amount - {high.BidAmount}</p>
+            <p>Deposit - {high.BidAmount * 0.4}</p>
+            <p>Due - {(high.BidAmount * 0.4) + high.BidAmount}</p>
+        </div>
+    )
+
+  })
   
 const BoardPrint = React.forwardRef((props, ref) => {
-  
+
+
   return(
-    <div className="p-4 font-fira h-screen" ref={ref}>
-    <h2 className='text-black'>{currentAuction}</h2>
+    <div className="p-7 font-fira h-screen" ref={ref}>
+    <h2 className='text-black'>Test Auction</h2>
+    <p>April 1, 2024</p>
     <table className="w-full text-sm text-black table-auto right-0">
         <thead className="w-full text-xs text-gray-700 uppercase bg-gray-50 dark:text-white">
             <tr>
                 <th scope="col" className="px-6 py-3 border-r-2 border-gray-400 ">
-                    Bujujgyugyujgygjgjh
+                    Bidder Number
                 </th>
                 <th scope="col" className="px-6 py-3 border-r-2 border-gray-400">
                     Bid Amount
@@ -69,19 +92,113 @@ const BoardPrint = React.forwardRef((props, ref) => {
             </tr>
         </thead>
         <tbody>
-        {bids.map((bid, index) => (
-            <tr key={index}>
-                <td className="border-gray-400 border-r-2 px-6 py-4">{bid.Bidder}</td>
-                <td className="border-gray-400 border-r-2 px-6 py-4">{bid.BidAmount}</td>
-                <td className="border-gray-400 border-r-2 px-6 py-4">{bid.ToLead}</td>
-                <td className="border-gray-400 border-r-2 px-6 py-4">{bid.PerAcre}</td>
-                <td className="border-gray-400 border-r-2 px-6 py-4">{bid.High ? 'Yes' : 'No'}</td>
-                <td className="border-gray-400 px-6 py-4">{bid.Tract}</td>
-            </tr>
-        ))}
-        </tbody>
-    </table>
-      
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="border-gray-400 border-r-2 px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            1
+                        </th>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td class="border-gray-400 border-r-2 px-6 py-4">
+                            $5
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td className=" border-gray-400 border-r-2 px-6 py-4">
+                            <span className='p-2 m-2'>1</span>
+                            <span className='p-2 m-2'>2</span>
+                        </td>
+                    </tr>
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="border-gray-400 border-r-2 px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            2
+                        </th>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td class="border-gray-400 border-r-2 px-6 py-4">
+                            $5
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td className=" border-gray-400 border-r-2 px-6 py-4">
+                            <span className=' p-2 m-2'>3</span>
+                            <span className=' p-2 m-2'>4</span>
+                            <span className=' p-2 m-2'>5</span>
+                        </td>
+                    </tr>
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="border-gray-400 border-r-2 px-6 py-4 bg-gray-200 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            3
+                        </th>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $100
+                        </td>
+                        <td class="border-gray-400 border-r-2 px-6 py-4">
+                            $5
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td className=" border-gray-400 border-r-2 px-6 py-4">
+                            <span className=' p-2 m-2'>7</span>
+                        </td>
+                    </tr>
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="border-gray-400 border-r-2 px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            4
+                        </th>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td class="border-gray-400 border-r-2 px-6 py-4">
+                            $5
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td className=" border-gray-400 border-r-2 px-6 py-4">
+                            <span className='p-2 m-2'>8</span>
+                            <span className='p-2 m-2'>9</span>
+                        </td>
+                    </tr>
+                    <tr className=" dark:bg-black">
+                        <th scope="row" className="border-gray-400 border-r-2 px-6 py-4 font-medium text-white-200 whitespace-nowrap dark:text-white">
+                            5
+                        </th>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td class="border-gray-400 border-r-2 px-6 py-4">
+                            $5
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $0.75
+                        </td>
+                        <td className="border-gray-400 border-r-2 px-6 py-4">
+                            $1
+                        </td>
+                        <td className=" border-gray-400 border-r-2 px-6 py-4">
+                            <span className=' p-2 m-2'>10</span>
+                        </td>
+                    </tr>
+
+                </tbody>
+        </table>
     </div>
   )
   
@@ -94,7 +211,26 @@ const BoardPrint = React.forwardRef((props, ref) => {
   return (
     <>
           <div className="p-4 sm:ml-64 font-fira dark:bg-gray-800 h-screen">
-            <h2 className='text-white'>{currentAuction}</h2>
+            <div>
+                <h2 className='text-white'>{currentAuction}</h2>
+                <ReactToPrint
+                content={() => ref.current}
+                trigger={() => (
+                    <button className='p-2 bg-red-600 text-white'>
+                            Print Report
+                    </button>
+                )}
+                />    
+                <ReactToPrint
+                content={() => ref.current}
+                trigger={() => (
+                    <button className='p-2 bg-red-600 text-white m-2'>
+                            Settlement
+                    </button>
+                )}
+                />    
+            </div>
+        
             <table className="w-full text-sm text-white right-0 border-collapse	">
                 <thead className="w-full text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-white">
                     <tr>
@@ -136,6 +272,10 @@ const BoardPrint = React.forwardRef((props, ref) => {
             <div style={{display: "none"}}>
                 <BoardPrint ref={ref}></BoardPrint>
             </div>
+            <div style={{display: "none"}}>
+                <SettlementPrint ref={ref}></SettlementPrint>
+            </div>
+          
             </div>
         </ >
   );
